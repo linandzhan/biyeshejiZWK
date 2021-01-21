5cebf8317e8626d6538474e26997dc96db7fce28
zhanweikai <zhanwk@ewhale.cn>
2021-01-21 11:24:06 +0800

    1.21

--- src/project/views/Welcome.vue
+++ src/project/views/Welcome.vue
@@ -1,21 +1,28 @@
 <template>
   <el-row class="page">
-    <el-col class="welcome" :span="10" :offset="8">{{$t('message.projectName')}}</el-col>
+    <el-col class="welcome" :span="10" :offset="8">{{
+      $t("message.projectName")
+    }}</el-col>
   </el-row>
 </template>
 
 <script>
-    export default {
-        name: "Welcome"
-    }
+export default {
+  name: "Welcome"
+};
 </script>
 
 <style lang="less" scoped>
-  .page {
+.page {
+  position: relative;
+  width: 100%;
+  background-image: url(../../assets/img/login-bg.jpg);
+  background-size: 100%;
+  background-repeat: no-repeat;
   .welcome {
-    margin-top: 200px;
-    font-size: 30px;
+    margin-top: 100px;
+    font-size: 50px;
     color: #333333;
   }
-  }
+}
 </style>
